77775c6420b9f3731f50d7fd16c0af6516d3adba
Sergey Golstinin <golstinin@gmail.com>
2023-12-15 16:00:00 -0500

    Update LeftSidebar and constants

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
 
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
   return (
     <section className='left_sidebar'>
      <nav className='flex flex-col gap-6'>
@@ -12,9 +18,14 @@ const LeftSidebar = () => {
         <h1 className='text-24 font-extrabold text-white max-lg:hidden'>Podcast</h1>
       </Link>
 
-       {sidebarLinks.map(({ route, label }) => { 
-          return <Link href={route}>
-          {label}
+       {sidebarLinks.map(({ route, label, imgURL}) => { 
+          const isActive = pathname === route || pathname.startsWith (`${route}/`);
+
+          return <Link href={route} key={label} className =
+          {cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start", {
+            'bg-nav-focus border-r-4 border-orange-1': isActive})}>
+             <Image src ={imgURL} alt={label} width={24} height={24}/>
+             <p>{label}</p>
           </Link>
        })
        }

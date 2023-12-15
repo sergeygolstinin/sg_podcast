408ee767093e8699d7fbd23d2dfd602189aeb775
Sergey Golstinin <golstinin@gmail.com>
2023-12-15 12:00:00 -0500

    Add authentication and root components

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,26 @@
+import { sidebarLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+const LeftSidebar = () => {
+  return (
+    <section className='left_sidebar'>
+     <nav className='flex flex-col gap-6'>
+      <Link href="/" className='flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center'>
+        <Image src="/icons/logo.svg" alt="logo" width={23} height={27}/> 
+        <h1 className='text-24 font-extrabold text-white max-lg:hidden'>Podcast</h1>
+      </Link>
+
+       {sidebarLinks.map(({ route, label }) => { 
+          return <Link href={route}>
+          {label}
+          </Link>
+       })
+       }
+     </nav>
+    </section>
+  )
+}
+
+export default LeftSidebar

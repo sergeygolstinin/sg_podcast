b38cd44064bf8bd337d66c3dd83dd2cc2ea035c3
Sergey Golstinin <golstinin@gmail.com>
2023-12-20 10:00:00 -0500

    Update root page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import PodcastCard from '@/components/PodcastCard'
 import { Button } from '@/components/ui/button'
+import { podcastData } from '@/constants'
 
 
 const Home = () => {
@@ -6,6 +8,16 @@ const Home = () => {
     <div className='mt-9 flex flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending Podcasts </h1>
+        {podcastData.map(({ id, title, description, imgURL }) => ( 
+
+          <PodcastCard 
+          key={id}
+          imgURL={imgURL}   
+          title={title}    
+          description={description}  
+          podcastId={id} 
+          />
+        ))}
       </section>
 
 

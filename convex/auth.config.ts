788b8cc5fb410a0526bf3797bc7ccddf8bb2db8f
Sergey Golstinin <golstinin@gmail.com>
2024-01-04 18:00:00 -0500

    Add Clerk related configurations and middleware

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+
+    providers: [
+      {
+        domain: "https://enjoyed-ape-70.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  export default authConfig;

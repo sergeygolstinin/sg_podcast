c7e74d9dad8038f6ed35e21822cb951bede04503
Sergey Golstinin <golstinin@gmail.com>
2024-01-24 10:00:00 -0500

    Update create-podcast page

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -28,25 +28,39 @@ import { useState } from "react"
 import { Textarea } from "@/components/ui/textarea"
 import GeneratePodcast from "@/components/GeneratePodcast"
 import GenerateThumbnail from "@/components/GenerateThumbnail"
+import { Loader } from "lucide-react"
+import { Id } from "@/convex/_generated/dataModel"
 
 const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
+  podcastTitle: z.string().min(2), 
+  podcastDescription: z.string().min(2),
 })
 
 const CreatePodcast = () => {
- const [voiceType, setVoiceType] = useState<string | null>(null) 
 
- const [isSubmitting, setisSubmitting] = useState(false);
+ const [imagePrompt, setImagePrompt] = useState('');
+ const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(null)
+ const [imageUrl, setImageUrl] = useState('');
+
+
+ const [audioUrl, setAudioUrl] = useState('');
+ const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(null)
+ const [audioDuration, setAudioDuration] = useState(0);
+
+ const [voiceType, setVoiceType] = useState<string | null>(null);
+ const [voicePrompt, setVoicePrompt] = useState('');
+
+ const [isSubmitting, setIsSubmitting] = useState(false);
+
 
 // 1. Define your form.
 const form = useForm<z.infer<typeof formSchema>>({
   resolver: zodResolver(formSchema),
   defaultValues: {
-    username: "",
+    podcastTitle: "",
+    podcastDescription: "",
   },
 })
 
@@ -69,9 +83,9 @@ function onSubmit(values: z.infer<typeof formSchema>) {
           name="podcastTitle"
           render={({ field }) => (
             <FormItem className="flex flex-col gp-2.5">
-              <FormLabel className="text-16 font-bold text-white-1">Username</FormLabel>
+              <FormLabel className="text-16 font-bold text-white-1">Title</FormLabel>
               <FormControl>
-                <Input className="input-class focus-visible:ring-orange-1"placeholder="SG Pro podcast" {...field} />
+                <Input className="input-class focus-visible:ring-offset-orange-1"placeholder="SG Pro podcast" {...field} />
               </FormControl>
               <FormMessage className="text-white-1"/>
           </FormItem>
@@ -85,7 +99,7 @@ function onSubmit(values: z.infer<typeof formSchema>) {
 
 
           <Select onValueChange={(value) => setVoiceType(value)}>
-            <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1')}>
+            <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1 focus:ring-offset-orange-1')}>
               <SelectValue placeholder="Select AI Voice" className="placeholder:text-gray-1"/>
             </SelectTrigger>
             <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-orange-1">
@@ -112,7 +126,7 @@ function onSubmit(values: z.infer<typeof formSchema>) {
             <FormItem className="flex flex-col gp-2.5">
               <FormLabel className="text-16 font-bold text-white-1">Description</FormLabel>
               <FormControl>
-                <Textarea className="input-class focus-visible:ring-orange-1"placeholder="Write a short podcast description" {...field} />
+                <Textarea className="input-class focus-visible:ring-offset-orange-1"placeholder="Write a short podcast description" {...field} />
               </FormControl>
               <FormMessage className="text-white-1"/>
           </FormItem>
@@ -121,7 +135,15 @@ function onSubmit(values: z.infer<typeof formSchema>) {
         </div>
 
         <div className="flex flex-col pt-10">
-          <GeneratePodcast />
+          <GeneratePodcast 
+           setAudioStorageId={setAudioStorageId}
+           setAudio={setAudioUrl}
+           voiceType={voiceType}
+           audio={audioUrl}
+           voicePrompt={voicePrompt}
+           setVoicePrompt={setVoicePrompt}
+           setAudioDuration={setAudioDuration}
+          />
 
           <GenerateThumbnail />
 
@@ -129,7 +151,8 @@ function onSubmit(values: z.infer<typeof formSchema>) {
             <Button type="submit" className="text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1">
             {isSubmitting ? ( 
                <>
-                 Submitting...
+                 Submitting
+                 <Loader size={20} className="animate-spin ml-2"/>
                 </>
             ) : (
               'Submit & Publish Podcast'

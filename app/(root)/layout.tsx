ca83d2aeaa57ce2249b52509b0a61387e51be432
Sergey Golstinin <golstinin@gmail.com>
2024-02-07 10:00:00 -0500

    Update root layout for OpenAI integration

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import LeftSidebar from "@/components/LeftSidebar";
 import MobileNav from "@/components/MobileNav";
 import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
+import { Toaster } from "@/components/ui/toaster"
 
 export default function RootLayout({
   children,
@@ -26,7 +27,8 @@ export default function RootLayout({
                     <MobileNav/>
                   </div>   
                   <div className="flex flex-col md:pb-14">
-                    Toaster 
+                  <Toaster />
+                   
                     {children}
                   </div>                  
                </div>

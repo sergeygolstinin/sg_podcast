a77da80bc459d5a21f9d6dd923fadc8c8c2c8375
Sergey Golstinin <golstinin@gmail.com>
2024-02-21 12:00:00 -0500

    Update podcast details page

--- app/(root)/podcast/[podcastId]/page.tsx
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
 const PodcastDetails = ({ params }: {params: {podcastId: string } }) => {
   return (
-    <p className='text-white-1'>PodcastDetails for {params.podcastId}</p>
+    <section className='flex w-full flex-col'>
+     <header className='mt-9 flex items-center justify-between'>
+      <h1 className='text-20 font-bold text-white-1'>
+           Currently playing
+      </h1>
+      <figure className='flex gap-3'>
+        <Image 
+          src="/icons/headphone.svg"
+          width={24}
+          height={24}
+          alt="headphone"
+        />
+       {/* <h2 className='text-16 font-bold text-white-1'>
+           {podcast?.views} 
+  </h2> */}
+      </figure>
+     </header>
+    </section>
+    
   )
 }
 
